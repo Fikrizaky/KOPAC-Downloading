608ad7b2ace175a1c2872ec8b3d9a054fa8fbf49
Fikrizaky <fikrizaky2711@gmail.com>
2017-10-13 07:49:29 +0700

    Delete Resizethumbar.js

--- Resizethumbar.js
+++
@@ -1,8 +0,0 @@
-function resizeThumbarlina(e,t){for(var s=document.getElementById(e),r=s.getElementsByTagName("img"),c=0;c<r.length;c++)r[c].src=r[c].src.replace(/\/s72\-c/,"/s"+t),r[c].width=t,r[c].height=t}resizeThumbarlina("PopularPosts1",400);
-$(document).ready(function(){
- $('.PopularPosts img').each(function(){
-  var srcUrl = $(this).attr('src');
-  $(this).attr('src', srcUrl.replace('default', 'maxresdefault'));
- });
-
-});
